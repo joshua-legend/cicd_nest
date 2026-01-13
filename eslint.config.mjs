a499dec457e1d8eaf088e9804e8da1f4bf4c94a4
joshua <iuttn123@gmail.com>
2026-01-13 14:57:54 +0900

    cicd2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,9 +26,14 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-explicit-any': 'off', // any 타입 사용 금지 규칙 해제
+      '@typescript-eslint/no-floating-promises': 'warn', // 프로미스 사용시 await/then 안하면 경고
+      '@typescript-eslint/no-unsafe-argument': 'warn', // any 등 안전하지 않은 타입을 함수 인자로 넘기면 경고
+      '@typescript-eslint/no-unused-vars': 'off', // 사용하지 않는 변수에 대한 경고 해제
+      '@typescript-eslint/no-unsafe-assignment': 'off', // any 등 안전하지 않은 값 할당 규칙 해제
+      '@typescript-eslint/no-unsafe-call': 'off', // any 등 안전하지 않은 값의 함수 호출 규칙 해제
+      '@typescript-eslint/no-unsafe-member-access': 'off', // any 등 안전하지 않은 값의 멤버 접근 규칙 해제
+      '@typescript-eslint/no-unsafe-return': 'off', // any 등 안전하지 않은 값의 반환 규칙 해제
     },
   },
 );
